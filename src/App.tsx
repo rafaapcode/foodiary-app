15a4d8586f06d2b2bcda12016436a11a39a35e3d
rafaapcode <rafaap2003@gmail.com>
2025-09-10 21:56:58 -0300

    feat: setting the fonts

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,24 @@
+import {
+  HostGrotesk_400Regular,
+  HostGrotesk_500Medium,
+  HostGrotesk_600SemiBold,
+  useFonts,
+} from '@expo-google-fonts/host-grotesk';
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 import { theme } from './ui/theme';
 
 export default function App() {
+  const [isFontsLoaded]  = useFonts({
+    HostGrotesk_400Regular,
+    HostGrotesk_500Medium,
+    HostGrotesk_600SemiBold,
+  });
+
+  if(!isFontsLoaded) {
+    return null;
+  }
+
   return (
     <View style={styles.container}>
       <Text>Open up App.tsx to start working on your app!</Text>
