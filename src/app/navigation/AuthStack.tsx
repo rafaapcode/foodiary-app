80149fe14a9fc5ec7c344178077c87cb582d84d4
rafaapcode <rafaap2003@gmail.com>
2025-09-20 15:29:30 -0300

    fix: fixing the types of the navigation

--- src/app/navigation/AuthStack.tsx
+++ src/app/navigation/AuthStack.tsx
@@ -1,3 +1,4 @@
+import { RouteProp } from '@react-navigation/native';
 import { createNativeStackNavigator, NativeStackNavigationProp, NativeStackScreenProps } from '@react-navigation/native-stack';
 import Greetings from '@ui/screens/Greetings';
 import Onboarding from '@ui/screens/Onboarding';
@@ -9,6 +10,7 @@ type AuthStackParamList = {
 
 export type AuthStackNavigationProps = NativeStackNavigationProp<AuthStackParamList>;
 export type AuthStackScreenProps<TRouteName extends keyof AuthStackParamList> = NativeStackScreenProps<AuthStackParamList, TRouteName>;
+export type AuthStackRouteProps<TRouteName extends keyof AuthStackParamList> = RouteProp<AuthStackParamList, TRouteName>;
 
 const Stack = createNativeStackNavigator<AuthStackParamList>();
 
