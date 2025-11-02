cf4179435983b23d01344f50dca6a5bf9e928579
Rafael Aparecido <rafaap2003@gmail.com>
2025-11-02 20:25:28 -0300

    feat: configuration the envs of app

--- src/app/services/Service.ts
+++ src/app/services/Service.ts
@@ -1,7 +1,8 @@
+import { env } from '@app/config/env';
 import axios from 'axios';
 
 export abstract class Service {
   protected static client =  axios.create({
-    baseURL: 'https://api.dev.foodiary.rafaapcode.com.br',
+    baseURL: env.api.url,
   });
 }
