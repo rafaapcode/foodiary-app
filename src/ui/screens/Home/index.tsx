53e2175b9c3c4c03680892dbac259703b7b8a6c9
Rafael Aparecido <rafaap2003@gmail.com>
2025-11-07 21:37:26 -0300

    fix: using the useAccount hook to get an account

--- src/ui/screens/Home/index.tsx
+++ src/ui/screens/Home/index.tsx
@@ -1,14 +1,16 @@
 import { useAuth } from '@app/contexts/AuthContext/useAuth';
+import { useAccount } from '@app/hooks/queries/useAccount';
 import { AppText } from '@ui/components/AppText';
 import { Button } from '@ui/components/Button';
 import { View } from 'react-native';
 
 const Home = () => {
   const { signOut } = useAuth();
+  const { account } = useAccount();
 
   return (
     <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-      <AppText>Home</AppText>
+      <AppText>Bem Vindo {account?.profile.name}</AppText>
       <Button onPress={signOut}>
         Sair
       </Button>
